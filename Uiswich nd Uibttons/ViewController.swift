4836c183ac989da6bf5ef9bfca6a50700b2a7b2a
THOTA NAGARAJU <naga.thota99@achiever-66.local>
2019-09-16 16:45:49 +0530

    Initial Commit

---
+++ Uiswich nd Uibttons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uiswich nd Uibttons
+//
+//  Created by THOTA NAGARAJU on 9/16/19.
+//  Copyright © 2019 THOTA NAGARAJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Uiswich nd UibttonsTests/Uiswich_nd_UibttonsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Uiswich_nd_UibttonsTests.swift
+//  Uiswich nd UibttonsTests
+//
+//  Created by THOTA NAGARAJU on 9/16/19.
+//  Copyright © 2019 THOTA NAGARAJU. All rights reserved.
+//
+
+import XCTest
+@testable import Uiswich_nd_Uibttons
+
+class Uiswich_nd_UibttonsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
